c106e41e3768aae1e93374c7b80d1a7a4be78934
Aysegul karimis <aysegulkarimis@WTMBP-Aysegul-Karimis.local>
2022-05-11 17:50:35 -0400

    edited the setup Wiremock Maping

--- NewsUITests/Tests/BaseTest.swift
+++ NewsUITests/Tests/BaseTest.swift
@@ -11,96 +11,51 @@ import WiremockClient
 
 class BaseTest: XCTestCase {
     let app = XCUIApplication()
-   
+    
+    private func setUpWiremockMapping(urlPath: String, queryParameter: String, queryParameterValue: String, fileName: String) {
+        do {
+            try WiremockClient.postMapping(stubMapping:
+                                            StubMapping.stubFor(requestMethod: .ANY, urlMatchCondition: .urlPathEqualTo, url: urlPath).withQueryParam(queryParameter, matchCondition: .equalTo, value: queryParameterValue)
+                .willReturn(
+                    ResponseDefinition()
+                        .withLocalJsonBodyFile(fileName, in: Bundle(for: type(of: self)))
+                )
+            )
+        } catch {
+            XCTFail()
+        }
+    }
+    
+    private func setUpWiremockMapping(urlPath: String, queryParameterOne: String, queryParameterOneValue: String, queryParameterTwo: String, queryParameterTwoValue: String, fileName: String) {
+        do {
+            try WiremockClient.postMapping(stubMapping:
+                                            StubMapping.stubFor(requestMethod: .ANY, urlMatchCondition: .urlPathEqualTo, url: urlPath).withQueryParam(queryParameterOne, matchCondition: .equalTo, value: queryParameterOneValue).withQueryParam(queryParameterTwo, matchCondition: .equalTo, value: queryParameterTwoValue)
+                .willReturn(
+                    ResponseDefinition()
+                        .withLocalJsonBodyFile(fileName, in: Bundle(for: type(of: self)))
+                )
+            )
+        } catch {
+            XCTFail()
+        }
+    }
+    
     override func setUp() {
         continueAfterFailure = false
-        do {
-            try WiremockClient.postMapping(stubMapping:
-                                            StubMapping.stubFor(requestMethod: .ANY, urlMatchCondition: .urlPathEqualTo, url: "/top-headlines").withQueryParam("country", matchCondition: .equalTo, value: "US")
-                .willReturn(
-                    ResponseDefinition()
-                        .withLocalJsonBodyFile("top_headlines", in: Bundle(for: type(of: self)))
-            )
-            )
-        } catch {
-            XCTFail()
-        }
-        do {
-            try WiremockClient.postMapping(stubMapping:
-                                            StubMapping.stubFor(requestMethod: .ANY, urlMatchCondition: .urlPathEqualTo, url: "/top-headlines").withQueryParam("country", matchCondition: .equalTo, value: "US").withQueryParam("category", matchCondition: .equalTo, value: "business")
-                .willReturn(
-                    ResponseDefinition()
-                        .withLocalJsonBodyFile("business_category", in: Bundle(for: type(of: self)))
-            )
-            )
-        } catch {
-            XCTFail()
-        }
-        do {
-            try WiremockClient.postMapping(stubMapping:
-                                            StubMapping.stubFor(requestMethod: .ANY, urlMatchCondition: .urlPathEqualTo, url: "/top-headlines").withQueryParam("country", matchCondition: .equalTo, value: "US").withQueryParam("category", matchCondition: .equalTo, value: "entertainment")
-                .willReturn(
-                    ResponseDefinition()
-                        .withLocalJsonBodyFile("entertainment_category", in: Bundle(for: type(of: self)))
-            )
-            )
-        } catch {
-            XCTFail()
-        }
-        do {
-            try WiremockClient.postMapping(stubMapping:
-                                            StubMapping.stubFor(requestMethod: .ANY, urlMatchCondition: .urlPathEqualTo, url: "/top-headlines").withQueryParam("country", matchCondition: .equalTo, value: "US").withQueryParam("category", matchCondition: .equalTo, value: "general")
-                .willReturn(
-                    ResponseDefinition()
-                        .withLocalJsonBodyFile("general_category", in: Bundle(for: type(of: self)))
-            )
-            )
-        } catch {
-            XCTFail()
-        }
-        do {
-            try WiremockClient.postMapping(stubMapping:
-                                            StubMapping.stubFor(requestMethod: .ANY, urlMatchCondition: .urlPathEqualTo, url: "/sources").withQueryParam("country", matchCondition: .equalTo, value: "US").withQueryParam("language", matchCondition: .equalTo, value: "en")
-                .willReturn(
-                    ResponseDefinition()
-                        .withLocalJsonBodyFile("sources", in: Bundle(for: type(of: self)))
-            )
-            )
-        } catch {
-            XCTFail()
-        }
-        do {
-            try WiremockClient.postMapping(stubMapping:
-                                            StubMapping.stubFor(requestMethod: .ANY, urlMatchCondition: .urlPathEqualTo, url: "/everything").withQueryParam("sources", matchCondition: .equalTo, value: "abc-news").withQueryParam("language", matchCondition: .equalTo, value: "en")
-                .willReturn(
-                    ResponseDefinition()
-                        .withLocalJsonBodyFile("everything_abc", in: Bundle(for: type(of: self)))
-            )
-            )
-        } catch {
-            XCTFail()
-        }
-        do {
-            try WiremockClient.postMapping(stubMapping:
-                                            StubMapping.stubFor(requestMethod: .ANY, urlMatchCondition: .urlPathEqualTo, url: "/everything").withQueryParam("language", matchCondition: .equalTo, value: "en").withQueryParam("sources", matchCondition: .equalTo, value: "ars-technica")
-                .willReturn(
-                    ResponseDefinition()
-                        .withLocalJsonBodyFile("everything_ars_technica", in: Bundle(for: type(of: self)))
-            )
-            )
-        } catch {
-            XCTFail()
-        }
-        
-        
-        
+        setUpWiremockMapping(urlPath: "/top-headlines", queryParameter: "country", queryParameterValue: "US", fileName: "top_headlines")
+        setUpWiremockMapping(urlPath: "/top-headlines", queryParameterOne: "country", queryParameterOneValue: "US", queryParameterTwo: "category", queryParameterTwoValue: "business", fileName: "business_category")
+        setUpWiremockMapping(urlPath: "/top-headlines", queryParameterOne: "country", queryParameterOneValue: "US", queryParameterTwo: "category", queryParameterTwoValue: "entertainment", fileName: "entertainment_category")
+        setUpWiremockMapping(urlPath: "/top-headlines", queryParameterOne: "country", queryParameterOneValue: "US", queryParameterTwo: "category", queryParameterTwoValue: "general", fileName: "general_category")
+        setUpWiremockMapping(urlPath: "/sources", queryParameterOne: "country", queryParameterOneValue: "US", queryParameterTwo: "language", queryParameterTwoValue: "en", fileName: "sources")
+        setUpWiremockMapping(urlPath: "/everything", queryParameterOne: "sources", queryParameterOneValue: "abc-news", queryParameterTwo: "language", queryParameterTwoValue: "en", fileName: "everything_abc")
+        setUpWiremockMapping(urlPath: "/everything", queryParameterOne: "language", queryParameterOneValue: "en", queryParameterTwo: "sources", queryParameterTwoValue: "ars-technica", fileName: "everything_ars_technica")
         
         app.launch()
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-
+    
 }
